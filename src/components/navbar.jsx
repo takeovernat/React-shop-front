afdeb8376d79d5e5efbb49e29b6536fbbb8355bb
takeovernat <natnaelteshome9@gmail.com>
2024-05-27 16:34:42 -0500

    change path for shop

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links">
-        <Link to="/"> Shop </Link>
+        <Link to="/shop"> Shop </Link>
         <Link to="/contact"> Contact </Link>
         <Link to="/cart">
           <ShoppingCart size={32} />
